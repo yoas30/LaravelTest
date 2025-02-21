8ccbb06dde943f7381cdbf468cf311f5fd81635e
yoas30 <yoas.rinatan30@gmail.com>
2025-02-22 01:48:56 +0700

    modified:   resources/views/apidata.blade.php

--- resources/views/apidata.blade.php
+++ resources/views/apidata.blade.php
@@ -1,12 +1,3 @@
-{{-- @if(isset($data))
-    @foreach ($data as $key => $value)
-        <p>{{ $key }}: {{ json_encode($value) }}</p>
-    @endforeach
-@else
-    <p>Data tidak tersedia</p>
-@endif --}}
-
-
 <!DOCTYPE html>
 <html lang="id">
 <head>
@@ -25,7 +16,7 @@
     @endif
 
     {{-- Dropdown --}}
-    <select>
+    {{-- <select>
         <option value="">Pilih Title</option>
         @if(isset($data['carts']))
             @foreach ($data['carts'] as $cart)
@@ -34,7 +25,61 @@
                 @endforeach
             @endforeach
         @endif
+    </select> --}}
+    {{-- Menampilkan gambar dari setiap produk dalam dropdown --}}
+    {{-- @if(isset($data['carts']))
+        <div>
+            @foreach ($data['carts'] as $cart)
+                @foreach ($cart['products'] as $product)
+                    <div>
+                        <img src="{{ $product['thumbnail'] }}" alt="{{ $product['title'] }}" width="100">
+                    </div>
+                @endforeach
+            @endforeach
+        </div>
+    @endif --}}
+   
+    <h1>Pilih Produk</h1>
+
+    {{-- Dropdown untuk memilih produk --}}
+    <select id="productDropdown">
+        <option value="" data-image="">Pilih Title</option>
+        @if(isset($data['carts']))
+            @foreach ($data['carts'] as $cart)
+                @foreach ($cart['products'] as $product)
+                    <option value="{{ $product['title'] }}" data-image="{{ $product['thumbnail'] }}">
+                        {{ $product['title'] }}
+                    </option>
+                @endforeach
+            @endforeach
+        @endif
     </select>
+
+    {{-- Tombol Submit --}}
+    <button onclick="showImage()">Tampilkan Gambar</button>
+
+    {{-- Tempat menampilkan gambar --}}
+    <div id="imageContainer" style="margin-top: 20px;">
+        <img id="productImage" src="" alt="Pilih produk terlebih dahulu" width="200" style="display: none;">
+    </div>
+
+    <script>
+        function showImage() {
+            var dropdown = document.getElementById("productDropdown"); // Ambil dropdown
+            var image = document.getElementById("productImage"); // Ambil elemen gambar
+        
+            var selectedOption = dropdown.options[dropdown.selectedIndex]; // Ambil opsi yang dipilih
+            var imageUrl = selectedOption.getAttribute("data-image"); // Ambil atribut 'data-image'
+        
+            if (imageUrl) {
+                image.src = imageUrl; // Ubah gambar sesuai pilihan
+                image.style.display = "block"; // Tampilkan gambar
+            } else {
+                alert("Silakan pilih produk terlebih dahulu."); // Jika belum memilih, munculkan alert
+                image.style.display = "none"; // Sembunyikan gambar
+            }
+        }
+    </script>
 </body>
 </html>
 
